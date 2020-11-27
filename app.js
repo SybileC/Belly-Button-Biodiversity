04b098597c7a0c21ce2aebdbe7e89afdc2d4a8f3
SybileC <sybile.cherenfant@gmail.com>
2020-11-27 01:12:32 -0500

    Edited app.js file

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-d3.json("samples.json").then(function(data) {
-    console.log(data.names);
-    console.log(data.metadata);
-    console.log(data.samples);
-});
+
+function buildPlot() {
+
+    d3.json("samples.json").then(function(data) {
+    
+        // console.log(data.dataset.names);
+        // console.log(data.dataset.metadata);
+        // console.log(data.dataset.samples);
+    
+        // var names = data.dataset.names;
+        // var metadata = data.metadata;
+        console.log(data)
+    });
+
+    // var trace1 = {
+    //     type: "scatter",
+    //     mode: "lines",
+    //     name: name,
+    //     x: dates,
+    //     y: closingPrices,
+    //     line: {
+    //       color: "#17BECF"
+    //     }
+    //   };
+  
+}
