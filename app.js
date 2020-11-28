761c2167a7c51a286cc1bc9346456ac235389eae
SybileC <sybile.cherenfant@gmail.com>
2020-11-28 14:08:46 -0500

    Creating horizontal bar chart

--- app.js
+++ app.js
@@ -6,19 +6,25 @@ d3.json("samples.json").then((data) => {
     // console.log(data)
 });
 
-var names = data.dataset.names;
-var metadata = data.metadata;
-var samples = data.samples;
+// var names = data.names;
+// var metadata = data.metadata;
+// var samples = data.samples;
 
-// var trace1 = {
-//     type: "scatter",
-//     mode: "lines",
-//     name: name,
-//     x: dates,
-//     y: closingPrices,
-//     line: {
-//       color: "#17BECF"
-//     }
-//   };
+var sample_values = data.samples.map(row => row[3])
+var otu_ids = data.samples.map(row => row[1])
+var otu_labels = data.samples.map(row => row[2])
 
-// }
+var trace1 = {
+    type: "bar",
+    x: sample_values,
+    y: otu_labels,
+    orientation: "h"
+  };
+
+  var data = [trace1]
+
+  var layout = {
+      title: 'Top 10 OTUs Found in Individuals',
+    //   xaxis: sample_values,
+    //   yaxis: otu_labels,
+  };
