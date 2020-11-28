90b30ab0f581b0c182dfde51c1bf94eab32c880b
SybileC <sybile.cherenfant@gmail.com>
2020-11-28 15:53:00 -0500

    Creating horizontal bar chart

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 
-function init() {
+// function init() {
     d3.json("samples.json").then((data) => {
     
         console.log(data.names);
         console.log(data.metadata);
         console.log(data.samples);
-        // console.log(data)
+        // console.log(data)  
+        
+        var sample_values = data.samples.map(row => row[3])
+        var otu_ids = data.samples.map(row => row[1])
+        var otu_labels = data.samples.map(row => row[2])
+
+        var trace1 = {
+            type: "bar",
+            x: sample_values,
+            y: otu_labels,
+            orientation: "h"
+        };
+
+        var data = [trace1]
+
+        var layout = {
+        title: 'Top 10 OTUs Found in Individuals',
+    //   xaxis: sample_values,
+    //   yaxis: otu_labels,
+        };
+
+        Plotly.newPlot("bar", data, layout);
     });
 
 // var names = data.names;
 // var metadata = data.metadata;
 // var samples = data.samples;
 
-    var sample_values = data.samples.map(row => row[3])
-    var otu_ids = data.samples.map(row => row[1])
-    var otu_labels = data.samples.map(row => row[2])
-
-    var trace1 = {
-        type: "bar",
-        x: sample_values,
-        y: otu_labels,
-        orientation: "h"
-    };
-
-    var data = [trace1]
-
-    var layout = {
-        title: 'Top 10 OTUs Found in Individuals',
-    //   xaxis: sample_values,
-    //   yaxis: otu_labels,
-    };
-
-    Plotly.newPlot("bar-plot", data, layout);
-};
+  
+// };
