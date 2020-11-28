f41c9707f56f9ca41f247b3015ab15086cc6fa74
SybileC <sybile.cherenfant@gmail.com>
2020-11-28 12:08:07 -0500

    Deleted and reloaded files

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-
-function buildPlot() {
-
-    d3.json("samples/samples.json").then((data) => {
+d3.json("samples.json").then((data) => {
     
-        // console.log(data.dataset.names);
-        // console.log(data.dataset.metadata);
-        // console.log(data.dataset.samples);
-    
-        // var names = data.dataset.names;
-        // var metadata = data.metadata;
-        console.log(data)
-    });
+    console.log(data.names);
+    console.log(data.metadata);
+    console.log(data.samples);
+    // console.log(data)
+});
 
-    // var trace1 = {
-    //     type: "scatter",
-    //     mode: "lines",
-    //     name: name,
-    //     x: dates,
-    //     y: closingPrices,
-    //     line: {
-    //       color: "#17BECF"
-    //     }
-    //   };
-  
-}
+var names = data.dataset.names;
+var metadata = data.metadata;
+var samples = data.samples;
+
+// var trace1 = {
+//     type: "scatter",
+//     mode: "lines",
+//     name: name,
+//     x: dates,
+//     y: closingPrices,
+//     line: {
+//       color: "#17BECF"
+//     }
+//   };
+
+// }
