745a73181e2fcf09ba3644b4f46c927fedd4ac65
SybileC <sybile.cherenfant@gmail.com>
2020-11-28 23:45:57 -0500

    Updated js file

--- app.js
+++ app.js
@@ -4,32 +4,39 @@ function unpack(rows, index) {
     });
   }
   
-
-d3.json("data/samples.json").then(function(importedData) {
+// function buildPlot() {
+    d3.json("data/samples.json").then(function(importedData) {
     console.log(importedData.names);
     console.log(importedData.metadata);
     console.log(importedData.samples);
 
+    names = Object.values(importedData.names);
+    metadata = Object.values(importedData.metadata);
+    samples = Object.values(importedData.samples);
+
     // importedData.samples.forEach(sample => importedData.samples.filter(sampleObj => sampleObj.id == sample)[0].sample_values);
-    otu_labels = unpack(importedData.samples, 2);
-    sample_values = unpack(importedData.samples, 3);
-    otu_ids = unpack(importedData.samples, 1);
+    otu_labels = samples.map(row => row[2]);
+    // sample_values = Object.values(importedData.samples.sample_values);
+    // otu_ids = Object.values(importedData.samples.otu_ids);
 
-    console.log(otu_ids)
+    console.log(otu_labels);
+    // trace1 = {
+    //     type: "bar",
+    //     x: sample_values,
+    //     y: otu_ids,
+    //     text: otu_labels,
+    //     orientation: "h"
+    // };
 
-    trace1 = {
-        type: "bar",
-        x: sample_values,
-        y: otu_ids,
-        text: otu_labels,
-        orientation: "h"
-    };
+    // data = [trace1];
 
-    data = [trace1];
+    // layout = {
+    //     title: 'Top 10 OTUs Found in Individuals'
+    // }
 
-    layout = {
-        title: 'Top 10 OTUs Found in Individuals'
-    }
+    // Plotly.newPlot("bar", data, layout);
+    });
+    // }
 
-    Plotly.newPlot("bar", data, layout);
-});
+
+// buildPlot();
