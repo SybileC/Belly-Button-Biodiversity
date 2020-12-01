4d12719e0eb5e36fcbc6e834b2d8afe1ad27ef21
SybileC <sybile.cherenfant@gmail.com>
2020-12-01 02:11:03 -0500

    Working on horizontal bar

--- app.js
+++ app.js
@@ -15,32 +15,52 @@ d3.json("data/samples.json").then(function(importedData) {
   metadata = importedData.metadata;
   samples = importedData.samples;
 
-  init () {
-      trace1 = {
-      type: "bar",
-      x: sample['sample_values'],
-      y: otu_ids,
-      text: otu_labels,
-      orientation: "h"
-  };
-  
-      data = [trace1];
-  
-      layout = {
-          title: 'Top 10 OTUs Found in Individuals'
-      }
-  
-      Plotly.newPlot("bar", data, layout);
-      }
-
-  d3.selectAll("#selDataset").on("onchange", getSample);
-      
-  function getSample() {
-      //   return sample.id === 940;
-      dropdownMenu = d3.select("#selDataset").property("value");
-      // data.samples.filter(sampleObj => sampleObj.id == sample)[0].otu_ids
-
+  function filterSample(sample) {
+        return sample.id == 940;
   }
+
+  filteredSample = samples.filter(filterSample);
+
+  console.log(filteredSample);
+
+  sliced_sample = Array.prototype.slice.call(filteredSample.sample_values, 10);
+
+  console.log(sliced_sample);
+
+//   sample_values = filteredSample.map(sample => sample.sample_values);
+
+//   console.log(sample_values);
+
+//   sliced_values = sample_values.slice(0, 10);
+
+//   console.log(sliced_values);
+
+//   init () {
+//       trace1 = {
+//       type: "bar",
+//       x: sample['sample_values'],
+//       y: otu_ids,
+//       text: otu_labels,
+//       orientation: "h"
+//   };
+  
+//       data = [trace1];
+  
+//       layout = {
+//           title: 'Top 10 OTUs Found in Individuals'
+//       }
+  
+//       Plotly.newPlot("bar", data, layout);
+//       }
+
+//   d3.selectAll("#selDataset").on("onchange", getSample);
+      
+//   function getSample() {
+//       //   return sample.id === 940;
+//       dropdownMenu = d3.select("#selDataset").property("value");
+//       // data.samples.filter(sampleObj => sampleObj.id == sample)[0].otu_ids
+
+//   }
 
 //   filteredSamples = samples.filter(getSample);
 
